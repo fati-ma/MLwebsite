4c405e2ffc85d6d2357e9d9823d2485bf51ab56c
fati-ma <fatima.atiyya@yahoo.com>
2020-09-23 16:32:05 +0300

    Updated for lab08

--- js/script.js
+++ js/script.js
@@ -1,33 +1,55 @@
-var name = prompt("Welcome! Please let us know your name");
-console.log(name);
-
-var welcome = alert("Hello "+name+"! "+"We hope you like our blog!");
-console.log(welcome);
-
-var student = prompt(name+" are you a student? (Y/N)");
-console.log(student);
-
-var state;
-var required;
-var stateStudent;
-var message= 'You are ';
 
 function state() {
-if (student == 'Y' || student =='y'){
-    state = 'Student';
-} else if (student == 'N' || student =='n') {
-    state = 'Not Student';
-} else{
-    required=prompt(name+", please answer the question with (Y/y or N/n).")
-}
-console.log(state);
-stateStudent = document.getElementById("stateId");
-stateStudent.textContent=message+state;
+
+    var name = prompt("Welcome! Please let us know your name");
+    console.log(name);
+
+    var welcome = alert("Hello " + name + "! " + "We hope you like our blog!");
+    console.log(welcome);
+
+    var student = prompt(name + " are you a student? (Y/N)");
+    console.log(student);
+
+    var fields = '';
+    var stateS='';
+    var required;
+    var stateStudent;
+
+    while (student !== "Y" && student !== "N") {
+
+        required = prompt(name + ", please answer the question with (Y or N) only.")
+    }
+
+    if (student == 'Y') {
+        stateS = 'Student';
+        stateStudent = document.getElementById("stateId");
+        stateStudent.textContent = 'You are ' + stateS;
+
+    } else if (student == 'N') {
+        stateS = 'Not Student';
+        stateStudent = document.getElementById("stateId");
+        stateStudent.textContent = 'You are ' + stateS;
+    }
+
+    var numberOfFields = prompt('how many fields do you want to know about?');
+    for (var i = 0; i < numberOfFields; i++){
+        fields = fields + ''+numberOfFields+' is the number of fields '+name+' is interested in...  ';
+        fields1 = document.getElementById("fieldsId");
+        fields1.textContent = fields;    
+    }
+
+    
+    console.log(stateS);
+    console.log(fields);
+
 
 }
 state();
 
-var opinion = function  (){
-    document.write('<p>Thank you '+name+' for visiting our website!')
+
+var opinion = function () {
+    document.write('<p>Thank you ' + name + ' for visiting our website!')
 }
 opinion();
+
+
